dfe5a1ec525eb4ad49d303434f9a324b27f2fd90
Kevin Nguyen <test@email>
2020-10-21 00:54:20 -0400

    clean up

--- create-gCal-event.js
+++ create-gCal-event.js
@@ -3,17 +3,16 @@ function getdate(info,tab) {
   if (info.menuItemId !== CONTEXT_MENU_ID) {
     return;
   }
-  console.log(info.selectionText + " was selected.");
-
   const parsedDate = Sugar.Date.create(info.selectionText, { fromUTC: true })
   const start = new Date(parsedDate);
   const end = new Date(parsedDate.getTime() + 30*60000);
   const eventStart = start.toJSON().replace(/-|:|\.\d\d\d|Z/g,"");
   const eventEnd =  end.toJSON().replace(/-|:|\.\d\d\d|Z/g,"");
-  
-  chrome.tabs.create({  
-    url: "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=" + eventStart + "%2f" + eventEnd
-  });
+
+    chrome.tabs.create({  
+      url: "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=" + eventStart + "%2f" + eventEnd
+    });
+
 }
 chrome.contextMenus.create({
   title: "Create Event", 
