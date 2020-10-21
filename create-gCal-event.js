30b95b1f7fbfd085ce75cdf6eb87942ea0fb1910
kevin-knguyen <knguyen@west.com>
2020-10-21 09:18:01 -0400

    added check for valid dates

--- create-gCal-event.js
+++ create-gCal-event.js
@@ -4,14 +4,20 @@ function getdate(info,tab) {
     return;
   }
   const parsedDate = Sugar.Date.create(info.selectionText, { fromUTC: true })
-  const start = new Date(parsedDate);
-  const end = new Date(parsedDate.getTime() + 30*60000);
-  const eventStart = start.toJSON().replace(/-|:|\.\d\d\d|Z/g,"");
-  const eventEnd =  end.toJSON().replace(/-|:|\.\d\d\d|Z/g,"");
+  console.log("parsedDate: " + parsedDate);
+  if (parsedDate == "Invalid Date") {
+    console.log("Selection is not a recognized date.");
+    alert("That doesn't look like a date.")
+  } else {
+    const start = new Date(parsedDate);
+    const end = new Date(parsedDate.getTime() + 30*60000);
+    const eventStart = start.toJSON().replace(/-|:|\.\d\d\d|Z/g,"");
+    const eventEnd =  end.toJSON().replace(/-|:|\.\d\d\d|Z/g,"");
 
     chrome.tabs.create({  
       url: "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=" + eventStart + "%2f" + eventEnd
     });
+  }
 
 }
 chrome.contextMenus.create({
